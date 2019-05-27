954eea25cfcb92fea5b482aa18eaefd1956e7a70
DARBOIS Nicolas <GB5282@banque.intra.groupama.fr>
2019-05-27 16:18:53 +0200

    added functionnality to anonymizer + refacto class

--- src/fb_duckling/anonymizer.py
+++ src/fb_duckling/anonymizer.py
@@ -6,8 +6,8 @@ import re
 class Anonymizer(BaseClass):
 
     default_anonymization_dict = {
-        "email": "personal_email",
-        "phone-number": "personal_phone_number"
+        "email": "email",
+        "phone-number": "phone"
     }
 
     def __init__(self, anonymization_dict=default_anonymization_dict,
@@ -17,13 +17,46 @@ class Anonymizer(BaseClass):
         self.duckling = Duckling(*args, **kwargs)
         self.anonymization_dict = anonymization_dict
 
-    def __call__(self, text, locale=None):
+    def __call__(self, text, ignore_exp=None, fixed_len=True, locale=None):
+        result = self._duckling_request(text, locale)
+        anonymized_text = self._anonymize(
+            result, text, ignore_exp=None, fixed_len=True, locale=None)
+        return anonymized_text
 
-        result = self.duckling.request(text, locale=locale or self.locale)
+    def _anonymize(self, result, text, ignore_exp=[], fixed_len=True, special_char="/"):
+        """
+        Text anonymization
+        Warning: If fixed_len=False, it allows padding which use a special char,
+                it might be incompatible with existing Tokenizer.
 
+        :param result: result from duckling request
+        :param text: text to anonymize
+        :param ignore: list of value to ignore if found in result
+        :param fixed_len: padding is disabled which means there is no indication 
+                        on the length of the input data
+        :param special_char: char to use in padding function
+
+        :return: The anonymized version of the text
+        """
         anonymized_text = text
         for res in result:
-            if res["dim"] in self.anonymization_dict.keys():
-                anonymized_text = re.sub(res["body"], self.anonymization_dict[res["dim"]], anonymized_text)
+            if fixed_len:
+                if res["dim"] in self.anonymization_dict.keys():
+                    if not any(exp in res["body"] for exp in ignore_exp):
+                        anonymized_text = re.sub(
+                            res["body"], "personal_" + self.anonymization_dict[res["dim"]], anonymized_text)
+            else:
+                if res["dim"] in self.anonymization_dict.keys():
+                    if not any(exp in res["body"] for exp in ignore_exp):
+                        padding_len = len(res["body"])
+                        anonymized_text = re.sub(
+                            res["body"], self._add_padding(self.anonymization_dict[res["dim"]], padding_len, special_char), anonymized_text)
 
         return anonymized_text
+
+    def _duckling_request(self, text, locale=None):
+        result = self.duckling.request(text, locale=locale or self.locale)
+        return result
+
+    def _add_padding(self, key, length, special_char):
+        return key.center(length, special_char)
--- tests/fb_duckling/test_anonymizer.py
+++ tests/fb_duckling/test_anonymizer.py
@@ -12,12 +12,75 @@ class TestAnonymizer(object):
     def test_constructor(self, anonymizer):
 
         assert anonymizer.default_anonymization_dict == {
-            "email": "personal_email",
-            "phone-number": "personal_phone_number"
+            "email": "email",
+            "phone-number": "phone"
         }
 
-    def test_call(self, anonymizer):
-
+    def test_anonymize(self, anonymizer):
         raw_text = "All work and no play makes jack@gmail.com a dull boy 0102030405"
-        anonymized_text = "All work and no play makes personal_email a dull boy personal_phone_number"
-        assert anonymizer(raw_text) == anonymized_text
+        anonymized_text = "All work and no play makes personal_email a dull boy personal_phone"
+        result = [{
+            'body': 'jack@gmail.com',
+            'start': 27,
+            'value': {'value': 'jack@gmail.com'},
+            'end': 41,
+            'dim': 'email',
+            'latent': False
+        },
+            {
+            'body': '0102030405',
+            'start': 53,
+            'value': {'value': '0102030405'},
+            'end': 63,
+            'dim': 'phone-number',
+            'latent': False
+        }]
+        assert anonymizer._anonymize(result, raw_text) == anonymized_text
+
+    def test_anonymize_ignore(self, anonymizer):
+        raw_text = "All work and no play makes jack@gmail.com a dull boy 0102030405"
+        anonymized_text = "All work and no play makes personal_email a dull boy 0102030405"
+        result = [{
+            'body': 'jack@gmail.com',
+            'start': 27,
+            'value': {'value': 'jack@gmail.com'},
+            'end': 41,
+            'dim': 'email',
+            'latent': False
+        },
+            {
+            'body': '0102030405',
+            'start': 53,
+            'value': {'value': '0102030405'},
+            'end': 63,
+            'dim': 'phone-number',
+            'latent': False
+        }]
+        assert anonymizer._anonymize(result, raw_text, ignore_exp=[
+                                     "0102030405"]) == anonymized_text
+
+    def test_anonymize_padding(self, anonymizer):
+        raw_text = "All work and no play makes jack@gmail.com a dull boy 0102030405"
+        anonymized_text = "All work and no play makes ////email///// a dull boy //phone///"
+        result = [{
+            'body': 'jack@gmail.com',
+            'start': 27,
+            'value': {'value': 'jack@gmail.com'},
+            'end': 41,
+            'dim': 'email',
+            'latent': False
+        },
+            {
+            'body': '0102030405',
+            'start': 53,
+            'value': {'value': '0102030405'},
+            'end': 63,
+            'dim': 'phone-number',
+            'latent': False
+        }]
+        assert anonymizer._anonymize(
+            result, raw_text, fixed_len=False) == anonymized_text
+
+    def test_request(self, anonymizer):
+        raw_text = "All work and no play makes jack@gmail.com a dull boy 0102030405"
+        assert anonymizer._duckling_request(raw_text) is not None
