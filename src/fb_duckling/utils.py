ab9e8ba7ef53bc8f6bc1ab454cfcc425a28b5896
Benjamin Breton <benjamin.breton@orangebank.com>
2019-05-28 17:44:05 +0200

    removed .env dependency

--- src/fb_duckling/utils.py
+++ src/fb_duckling/utils.py
@@ -11,9 +11,6 @@ if path.isfile(env_path):
 elif path.isfile(env_default_path):
     load_dotenv(env_default_path)
     print("loaded env variables from: {}".format(env_default_path))
-else:
-    raise Exception("No .env file found please make sure that a .env file of the .env.default file"
-                    "is present at the root of the project")
 
 
 def get_default_port():
