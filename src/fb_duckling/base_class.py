301a3feb0f795fcf13fe262ed4d465eb9dc2e947
NicolasDrb <42466785+FellowNico@users.noreply.github.com>
2019-12-09 14:52:46 +0100

    Update base_class.py
    Added timezone

--- src/fb_duckling/base_class.py
+++ src/fb_duckling/base_class.py
@@ -6,9 +6,11 @@ class BaseClass(object):
     default_locale = get_default_locale()  # en_US
     default_url = get_default_url()  # http://0.0.0.0
     default_port = get_default_port()  # 8000
+    default_timezone = get_default_timezone() # America/Los_Angeles
 
-    def __init__(self, locale=default_locale, url=default_url, port=default_port):
+    def __init__(self, locale=default_locale, url=default_url, port=default_port, timezone=default_timezone):
 
         self.locale = locale or "en_US"
         self.url = url or "http://0.0.0.0"
         self.port = port or 8000
+        self.timezone = timezone
